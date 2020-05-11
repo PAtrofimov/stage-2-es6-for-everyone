d74ab85b90d26a0c3f34e8ab287b4e0ab459c1ea
Peter Trofimov <ptrofimov7@gmail.com>
2020-05-11 11:54:31 +0300

    Implemented function showWinnerModal

--- src/javascript/components/modal/winner.js
+++ src/javascript/components/modal/winner.js
@@ -1,3 +1,19 @@
+import {showModal} from './modal';
+import {createElement} from '../../helpers/domHelper';
+
 export function showWinnerModal(fighter) {
-  // call showModal function 
+  // call showModal function
+  const title = `${fighter.name} wins!`;
+  const body = createElement({ tagName: 'p', className: 'modal-body' });
+  body.innerText = "My congratulations to you! Welcome to the next fight!";
+  const result = {
+    title,
+    bodyElement: body,
+    onClose: () => {
+      console.log(name);
+      window.location.reload();
+    },
+  };
+  showModal(result);
 }
+
